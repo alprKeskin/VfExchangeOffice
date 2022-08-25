b616b54964bf315b611af257a5495a59a3e4f95e
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-08-25 14:00:30 +0300

    ms_currency service is created

---
+++ ms_currency/src/test/java/io/github/alprkeskin/currency/MsCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alprkeskin.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
