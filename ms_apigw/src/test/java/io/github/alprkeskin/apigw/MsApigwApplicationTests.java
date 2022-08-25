13e893dbb40e3fe2b9bb2e2d699136aeb661e20f
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-08-25 13:56:21 +0300

    ms_apigw service is created

---
+++ ms_apigw/src/test/java/io/github/alprkeskin/apigw/MsApigwApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alprkeskin.apigw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsApigwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
