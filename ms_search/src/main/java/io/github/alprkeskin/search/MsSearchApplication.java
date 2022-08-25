41e03cbbaecc765ed9222c28e559f112c096aa31
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-08-25 14:03:56 +0300

    ms_search service is created

---
+++ ms_search/src/main/java/io/github/alprkeskin/search/MsSearchApplication.java
@@ -0,0 +1,13 @@
+package io.github.alprkeskin.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSearchApplication.class, args);
+    }
+
+}
---
+++ ms_search/src/test/java/io/github/alprkeskin/search/MsSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alprkeskin.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
