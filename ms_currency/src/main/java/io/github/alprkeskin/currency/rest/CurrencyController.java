cd10a0ace931c89d3c8d5997834648160049f740
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-09-03 17:35:59 +0300

    improvements on logs

--- ms_currency/src/main/java/io/github/alprkeskin/currency/rest/CurrencyController.java
+++ ms_currency/src/main/java/io/github/alprkeskin/currency/rest/CurrencyController.java
@@ -3,6 +3,7 @@ package io.github.alprkeskin.currency.rest;
 import io.github.alprkeskin.currency.model.CurrencyRates;
 import io.github.alprkeskin.currency.service.CurrencyRatesMediatorService;
 import io.github.alprkeskin.currency.utils.DateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,20 +13,24 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/currency")
+@Slf4j
 public class CurrencyController {
-
     @Autowired
     private CurrencyRatesMediatorService currencyRatesMediatorService;
 
     @GetMapping(value = {"", "/{requestedDate}"})
+    @CrossOrigin
     public ResponseEntity<CurrencyRates> getCurrencyRates(
             @PathVariable(value = "requestedDate", required = false) String requestedDate) {
+        log.info("CurrencyController::getCurrencyRates(String requestedDate = {} )", requestedDate);
         LocalDate desiredDate = requestedDate == null ? LocalDate.now() : DateUtil.getLocalDate(requestedDate);
         return ResponseEntity.ok(currencyRatesMediatorService.getCurrencyRates(desiredDate));
     }
 
     @GetMapping(value = "/currencies")
+    @CrossOrigin
     public ResponseEntity<Map> getCurrencies() {
+        log.info("CurrencyController::getCurrencies()");
         return ResponseEntity.ok(currencyRatesMediatorService.getCurrencies());
     }
 }
--- ms_search/src/main/java/io/github/alprkeskin/search/rest/SearchController.java
+++ ms_search/src/main/java/io/github/alprkeskin/search/rest/SearchController.java
@@ -3,6 +3,9 @@ package io.github.alprkeskin.search.rest;
 import io.github.alprkeskin.search.model.Currency;
 import io.github.alprkeskin.search.service.CurrencyService;
 import io.github.alprkeskin.search.utils.Utils;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +14,7 @@ import java.io.IOException;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+@Slf4j
 @RestController
 @RequestMapping("/search")
 public class SearchController {
@@ -18,13 +22,17 @@ public class SearchController {
     private CurrencyService currencyService;
 
     @GetMapping("/{currency_name}")
+    @CrossOrigin
     public ResponseEntity<Currency> getCurrency(@PathVariable(value = "currency_name") String currencyName) throws IOException {
+        log.info("SearchController::getCurrency()");
         return ok(currencyService.getCurrency(currencyName));
     }
 
     @DeleteMapping("/{date}/{currency_name}")
+    @CrossOrigin
     public void deleteCurrency(@PathVariable(value = "date") String date,
                                @PathVariable(value = "currency_name") String currencyName) {
+        log.info("SearchController::deleteCurrency(String date = {}, String currencyName = {} )", date, currencyName);
         String relatedId = Utils.getIdWithDateAndCurrencyName(currencyName, date);
         currencyService.delete(relatedId);
     }
